9c5adadc907f74de615be9ca3f47ebfba5daa090
100405882 <100405882@alumnos.uc3m.es>
2021-05-08 17:12:00 +0200

    Añadido los componentes y el routing
    Testeo ahora si funciona el routing en la version web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HorarioComponent } from './horario/horario.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'principal', component: PrincipalComponent },
+  { path: '', redirectTo: '/principal', pathMatch:'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'horario', component: HorarioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HorarioComponent } from './horario/horario.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorarioComponent,
+    PrincipalComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
